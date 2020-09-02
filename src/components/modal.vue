7a693a9a61db973a5180d16893c1f3b87784d706
Ruben Smit <r.smit@compliance-innovations.com>
2020-09-02 21:16:43 +0200

    Add sass

--- src/components/modal.vue
+++ src/components/modal.vue
@@ -19,32 +19,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-  .modal {
-    width: 100vw;
-    height: 100vw;
-    background-color: rgba(200, 200, 200, 0.5);
-    position:fixed;
-    visibility: hidden;
-    transition: visibility .3s, opacity .3s;
-    opacity: 0;
-    z-index: 999;
-  }
-
-  .modal--visible {
-    visibility: visible;
-    opacity: 1;
-  }
-
-  .modal__body {
-    margin-top: 20%;
-    padding: 30px;
-    padding-left: 330px;
-    background-color: white;
-  }
-
-  .modal__body button:not(:last-child) {
-    margin-right: 1px;
-  }
-</style>
