45099753c61afe68536cae813d7a5de0553f381e
Ruben Smit <RubenSmit@users.noreply.github.com>
2021-04-24 19:46:39 +0200

    Fix bank trading

---
+++ src/components/trade-action.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="trade-action">
+    <div class="trade-action__resource" :class="resource">
+      {{ ratio }}
+    </div>
+    <span>➔</span>
+    <div class="trade-action__target" :class="resource" v-if="target">
+      1
+    </div>
+    <button v-else v-for="resource in otherResources" :key="resource.id" class="resources-list__resource-button" :class="resource.type"  @click="createTrade(resource)">
+      1
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['resource', 'ratio', 'target'],
+  computed: {
+    otherResources: function () {
+      let resources = [
+        {type: "GRAIN", value: 1},
+        {type: "WOOL", value: 1},
+        {type: "ORE", value: 1},
+        {type: "STONE", value: 1},
+        {type: "WOOD", value: 1}
+      ];
+      resources.splice(resources.findIndex((resource) => resource.type === this.resource), 1);
+      return resources;
+    }
+  },
+  methods: {
+    createTrade: function (resource) {
+      this.$emit("addTrade", {from: this.resource, ratio: this.ratio, to: resource.type});
+    }
+  }
+}
+</script>
--- src/components/trade-view.vue
+++ src/components/trade-view.vue
@@ -6,10 +6,31 @@
       </h1>
     </div>
     <div class="force-discard-view__resources">
-      <h2 class="aforce-discard-view__resources-title">
+      <h2 class="force-discard-view__resources-title">
         {{ T("CURRENT_RESOURCES") }}
       </h2>
       <resources-list v-if="me" :resources="me.attributes.resources" size="medium" />
+    </div>
+    <div class="trade-view__select">
+      <h3 class="trade-view__result-title">
+        {{ T("TRADE_OPTIONS") }}
+      </h3>
+      <trade-action v-for="option in tradeOptions" @addTrade="addTrade" :key="option.resource" :resource="option.resource" :ratio="option.ratio"/>
+    </div>
+    <div class="trade-view__actions">
+      <h3 class="trade-view__result-title">
+        {{ T("TRADE_ACTIONS") }}
+      </h3>
+      <trade-action v-for="action in tradeActions" :key="action.resource" :resource="action.from" :ratio="action.ratio" :target="action.to"/>
+      <button v-if="tradeActions" class="trade-view__actions-clear-button" @click="clearActions">
+        {{ T("CLEAR_TRADE") }}
+      </button>
+    </div>
+    <div class="trade-view__result">
+      <h3 class="trade-view__result-title">
+        {{ T("TRADE_RESULT") }}
+      </h3>
+      <resources-list :resources="tradeResult" size="medium" />
     </div>
     <div class="force-discard-view__finish">
       <button class="force-discard-view__finish-button" @click="sendClientResponse">
@@ -21,26 +42,48 @@
 
 <script>
 import ResourcesList from "./resources-list";
+import TradeAction from "./trade-action";
 
 export default {
-  components: {ResourcesList},
+  components: {ResourcesList, TradeAction},
   props: ["me", "dev_mode"],
   data: function() {
     return {
+      tradeActions: []
     }
   },
   computed: {
+    tradeOptions: function () {
+      let options = [];
+      for (let i in this.tradeResult) {
+        let resource = this.tradeResult[i];
+        if (resource.value >= 4) {
+          options.push({resource: resource.type, ratio: 4});
+        }
+      }
+      return options;
+    },
+    tradeResult: function () {
+      let result = JSON.parse(JSON.stringify(this.me.attributes.resources));
+      for (let i in this.tradeActions) {
+        let action = this.tradeActions[i];
+        for (let j in result) {
+          if (result[j].type == action.from) result[j].value -= action.ratio
+          if (result[j].type == action.to) result[j].value += 1
+        }
+      }
+      return result;
+    }
   },
   methods: {
     sendClientResponse: function() {
-      let discardMessage = [];
-      for (let item in this.resourcesToDiscard) {
-        discardMessage.push({
-          "type": item,
-          "value": this.resourcesToDiscard[item]
-        });
-      }
-      this.$emit("clientResponse", discardMessage);
+      this.$emit("clientResponse", this.tradeActions);
+    },
+    addTrade: function(trade) {
+      this.tradeActions.push(trade);
+    },
+    clearActions: function() {
+      this.tradeActions = [];
     }
   }
 };
